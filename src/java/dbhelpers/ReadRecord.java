31e07a77ba6075317564261992549ddf4bcc3ae3
Evan <evan-stimmel@uiowa.edu>
2017-03-30 10:33:17 -0500

    push for mike

---
+++ src/java/dbhelpers/ReadRecord.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbhelpers;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Players;
+
+/**
+ *
+ * @author Evan Stimmel
+ */
+public class ReadRecord {
+    
+    private Connection conn;
+    private ResultSet results;
+    
+    private Players player = new Players();
+    private int playerID;
+
+    
+    public ReadRecord (int playerID) {
+    
+        Properties props = new Properties();  //MWC
+        InputStream instr = getClass().getResourceAsStream("dbConn.properties");
+        try {
+            props.load(instr);
+        } catch (IOException ex) {
+            Logger.getLogger(ReadRecord.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            instr.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ReadRecord.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        String driver = props.getProperty("driver.name");
+        String url = props.getProperty("server.name");
+        String username = props.getProperty("user.name");
+        String passwd = props.getProperty("user.password");
+        
+        this.playerID = playerID;
+        
+        
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ReadRecord.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            conn = DriverManager.getConnection(url, username, passwd);
+        } catch (SQLException ex) {
+            Logger.getLogger(ReadRecord.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+}
+    
+    public void doRead() {
+    
+        try {
+
+            String query = "SELECT * FROM players WHERE playerID =?";
+            
+            PreparedStatement ps = null;
+            try {
+                ps = conn.prepareStatement (query);
+            } catch (SQLException ex) {
+                Logger.getLogger(ReadRecord.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            ps.setInt(1, playerID);
+            
+            this.results = ps.executeQuery();
+            
+            this.results.next();
+            
+            player.setplayerID(this.results.getInt("playerID"));
+            player.setplayerName(this.results.getString("name"));
+            player.setjerseyNumber(this.results.getInt("number"));
+            player.setage(this.results.getInt("age"));
+            player.setsuperbowlWins(this.results.getInt("wins"));
+            
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(ReadRecord.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+}   
+    
+    public Players getPlayer(){
+       
+        return this.player;
+    
+}
+}
---
+++ web/updateForm.jsp
@@ -0,0 +1,43 @@
+<%-- 
+    Document   : updateForm
+    Created on : Mar 28, 2017, 5:27:16 PM
+    Author     : Evan Stimmel
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@page import="model.Players"%>
+<% Players player = (Players) request.getAttribute("player"); %>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="evancss.css" />
+        <title>Update Players</title>
+    </head>
+    <body>
+   
+        <h1>Update A New Player</h1>
+        
+        <form name="updateForm" action="updatePlayer" method="get">
+            
+            <label>Player ID:</label>
+            <input type="text" name="name" value="<%= player.getPlayerID() %>" />
+            <br>
+            <label>Player Name:</label>
+            <input type="text" name="name" value="<%= player.getPlayerName() %>" />
+            <br>
+            <label>Jersey Number:</label>
+            <input type="text" name="number" value="<%= player.getJerseyNumber() %>" />
+            <br>
+            <label>Age:</label>
+            <input type="text" name="age" value="<%= player.getAge() %>" />
+            <br>
+            <label>Super Bowl Wins:</label>
+            <input type="text" name="wins" value="<%= player.getSuperbowlWins() %>" />
+            <br>
+            <input type="submit" name="submit" value="Submit" />
+        </form>
+    </body>
+</html>
